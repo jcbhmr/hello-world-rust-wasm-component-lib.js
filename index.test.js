fe2c91c7ef4cdbae474f525c2d0c8a7c2fe3a6b1
Jacob Hummer <jcbhmr@outlook.com>
2023-12-27 23:28:55 +0000

    idea 1

---
+++ index.test.js
@@ -0,0 +1,38 @@
+import test from "node:test";
+import assert from "node:assert";
+import * as hello_world_rust_wasm_component_lib from "./hello_world_rust_wasm_component_lib.js";
+import {
+  greet,
+  greetMany,
+  run,
+  getReport,
+  computeArea,
+} from "./hello_world_rust_wasm_component_lib.js";
+
+console.log(hello_world_rust_wasm_component_lib);
+
+test("greet() works", () => {
+  console.log(greet("Alan Turing"));
+});
+
+test("greetMany() works", () => {
+  console.log(greetMany(["Alan Turing", "Ada Lovelace"]));
+});
+
+test("run() works", () => {
+  run();
+});
+
+test("getReport() works", () => {
+  console.log(getReport());
+});
+
+test("computeArea() works", () => {
+  console.log(
+    computeArea({
+      center: { x: 0, y: 0 },
+      radius: 6,
+    })
+  );
+  console.log("from JS", Math.PI * 6 * 6);
+});
---
+++ interfaces/example-hello-world-rust-wasm-component-lib-hello-world-rust-wasm-component-lib-interface.d.ts
@@ -0,0 +1,16 @@
+export namespace ExampleHelloWorldRustWasmComponentLibHelloWorldRustWasmComponentLibInterface {
+}
+export interface Report {
+  bouncyCastles: number,
+  funPercent: number,
+  catCount: number,
+  unicornNames: string[],
+}
+export interface Point {
+  x: number,
+  y: number,
+}
+export interface Circle {
+  center: Point,
+  radius: number,
+}
